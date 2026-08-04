853dd9cde35978d4e96f0a25c9cb16131571fe0a
ElliottCepin <elliottcepin5@gmail.com>
2026-08-03 17:27:04 -0700

    parser test passes

--- internal/spf/spf.go
+++ internal/spf/spf.go
@@ -1,7 +1,36 @@
 package spf
 
-func checkHost(txt_records []string) string {
+import (
+	"strings"
+)
 
+func parseRecord(txtRecord []string) []string {
+	txt := strings.Join(txtRecord, "")
+	
+	parsed := make([]string, 0)
+
+	var builder strings.Builder
+	for i := 0; i<len(txt); i++ {
+			
+		if (txt[i] == ' ') {
+			if (builder.Len() > 0) {
+				parsed = append(parsed, builder.String())
+				builder.Reset()
+			}
+		} else {
+			builder.WriteString(string(txt[i]))
+		}
+
+	}
+
+	if builder.Len() > 0 {
+		parsed = append(parsed, builder.String())
+	}
+
+	return parsed
+}
+
+func checkHost(txtRecords []string) string {
 	return "none"
 }
 
--- internal/spf/spf_test.go
+++ internal/spf/spf_test.go
@@ -11,8 +11,24 @@ func TestNoSPF(t *testing.T) {
 	// should return none
 	record, _ := dns.NewRR(`dmarc.example.com.       TXT     "v=DMARC1; p=quarantine; rua=mailto:dmarc@example.com"`)
 	records := []string{record.String()}
+
 	if s := checkHost(records); s != "none" {
 		t.Errorf("Expected 'none', got '%s'", s)
+	}
+
+}
+
+func TestParseSPF(t *testing.T) {
+	record, _ := dns.NewRR(`example.com.       TXT     "v=SPF1 +mx -all"`)
+	parsed := parseRecord(record.(*dns.TXT).Txt)
+
+	if parsed[0] != "v=SPF1" {
+		t.Errorf("Expected 'v=SPF1', got '%s'", parsed[0])
+	}
+
+	if len(parsed) != 3 {
+		t.Errorf("Expected length 3, got length %v", len(parsed))
+		t.Logf("%v", parsed)
 	}
 
 }
